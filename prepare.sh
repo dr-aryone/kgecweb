2bea7195f0aaca2a7563b7a7ff9ce048c2b63c08
biswajit <you@example.com>
2015-10-11 11:10:19 +0530

    prepare.sh for prepareing new dev machine

---
+++ prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Update your repository"
+sudo apt-get update
+
+echo "Install Python Software Properties"
+sudo apt-get install python-software-properties  -y
+
+echo "Jpeg Support for PIL"
+sudo apt-get install libjpeg-dev  -y 
+
+echo "Install libmysqlclient dev"
+sudo apt-get install libmysqlclient-dev -y
+
